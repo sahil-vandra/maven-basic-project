6e3d00cb8346df6d164e0228cdce656dcaba21ef
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-08-24 19:30:47 +0530

    first commit

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -3,6 +3,12 @@ package com.acme.basic;
 public class HelloWorld {
 
   void sayHello() {
+    int target = -5;
+    int num = 3;
+
+    target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+    target =+ num; // Noncompliant; target = 3
+    
     System.out.println("Hello World!");
   }
 
